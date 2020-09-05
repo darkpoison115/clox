0a0506e45a5d03488a1423f59c3e8871deb0ee8f
= <josecortez182@gmail.com>
2020-09-05 17:19:28 -0400

    Chapters 18, 19 and 20

--- memory.h
+++ memory.h
@@ -1,6 +1,14 @@
 #ifndef clox_memory_h
 #define clox_memory_h
 
+#include "common.h"
+#include "object.h"
+
+#define ALLOCATE(type,count) \
+    (type*)reallocate(NULL,0,sizeof(type) * (count));
+
+#define FREE(type,pointer) reallocate(pointer, sizeof(type),0)
+
 #define GROW_CAPACITY(capacity) \
     ((capacity) < 8 ? 8 : (capacity) * 2)
 
@@ -12,4 +20,5 @@
     reallocate(pointer, sizeof(type) * (oldCount), 0)
 
 void* reallocate(void* previous, size_t oldSite, size_t newSite);
+void freeObjects();
 #endif
